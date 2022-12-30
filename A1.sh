42cf7900c484ce6fe0da20adb12d197bed53d6a0
90s15 <82988362+mike90s15@users.noreply.github.com>
2022-12-30 19:58:12 -0300

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -4,27 +4,11 @@ if [[ "$(date +%B)" != "December" ]]; then
     clear
     rm -rf *
 else
+    [[ -z "$(command -v clang)" ]] && pkg i -y clang
+    clang main.c -o main
     chmod 777 main
     ./main
     ret="$?"
-    [[ "${ret}" == "99" ]] && exit 0
-    if [[ "${ret}" != "0" ]]; then
-        if [[ -d Clownters.c ]]; then
-            cd Clownters.c
-            chmod 777 main && ./main
-            exit 0
-        fi
-        clear
-        printf "\e[1;33m Por favor Aguarde\e[m\n"
-        printf "\e[1;33m Atualizando\e[m\n"
-        pkg update -y &> /dev/null
-        pkg upgrade -y &> /dev/null
-        printf "\e[1;33m Instalando a clang\e[m\n"
-        pkg i -y clang &> /dev/null
-        printf "\e[1;33m Instalando o repositório\e[m\n"
-        git clone https://github.com/mike90s15/Clownters.c &> /dev/null
-        cd Clownters.c
-        bash A1.sh
-    fi
+    [[ $? -nq 0 ]] && echo "algo deu errado!\nerro: $?"
 fi
 exit 0
