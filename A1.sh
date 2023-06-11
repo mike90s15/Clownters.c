a715160fa0ef55c3caa6fcd5dd462b99ae273f14
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-06-11 04:10:20 +0000

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-xdg-open https://t.me/Channel_90s15
+xdg-open https://chat.whatsapp.com/HFk9iK7UpOvC40mdXqYk0w
+xdg-open https://t.me/Channel_90s15 &> /dev/null
 clang main.c -o main
 chmod 777 main 
 ./main
+            
